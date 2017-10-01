8f607af2e552cdabc26366bfb578ca17a33f8385
Yamingue <32344953+Yamingue@users.noreply.github.com>
2017-10-01 19:06:57 +0200

    NbrPaire.java

---
+++ NbrPaire.java
@@ -0,0 +1,13 @@
+public class NbrPaire(){
+   import java.util.Scanner;
+   public static void main(String[] args){
+     Scanner var=new Scanner(system.in);
+     int x=var.nextInt();
+     int v=x%2;
+     if(v==0){
+       System.out.println("le nombre est paire");
+     }else{System.out.println("le nombre est impaire");}
+   
+   }
+
+}
