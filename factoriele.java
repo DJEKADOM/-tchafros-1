bba5a040958d23e89dd1e1eb44f6305289e94851
Yamingue <32344953+Yamingue@users.noreply.github.com>
2017-10-01 19:50:32 +0200

    factoriele.java
    TPE tchafros

---
+++ factoriele.java
@@ -0,0 +1,27 @@
+public class factoriel(){
+       import java.util.Scanner;
+       static void main(String[] args){
+       int fac=1;
+       int k=0;
+         System.out.println("entrer un nombre");
+         Scanner variable=new Scanner(sytem.in);
+         int x=variable.nextInt();
+              if(x>=0){
+                 if(x==0){
+                 System.out.println("le factoriel de "+x+" est 1");
+                 }else{
+                    for(int i=0; x-1;i++){
+                    k=k+1;
+                    fac=fac*k;
+                   
+                    }
+                    System.out.println('le factoriel de '+x+' est '+fac);
+                 }
+              }else{System.out.println("le factoriel nexiste pas");
+       
+       
+       }
+
+
+
+}
